548e2333ca284d327fbf18cd0bff44d9fe5c5da0
Wayne Edwards <waedwards42@yahoo.com>
2021-03-07 23:09:22 -0800

    added If Else While statements

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Assignment code here
 
+// Arrays for password entries data
 function GeneratePassword() {
     var lowerCaseSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     var upperCaseSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -7,10 +8,14 @@ function GeneratePassword() {
     var specialSet = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", "_", "?", "/", "<", ">", "[", "]"];
     var selectedArray = [];
 
+
     var passwordLength = getPasswordLength();
+    //End Arrays for password entries data
+
 
     var charTypeSelected = false;
 
+    //Whil, If and Else statements
     while (charTypeSelected == false) {
         var lowerCase = getChoice("lowercase");
         var upperCase = getChoice("uppercase");
@@ -38,11 +43,31 @@ function GeneratePassword() {
 
     var passwordString = "";
 
+
     for (var i = 0; i < passwordLength; i++) {
         passwordString += selectedArray[Math.floor(Math.random() * (selectedArray.length))];
     }
     return passwordString;
+}
 
+
+function getPasswordLength() {
+    var userChoice = 0;
+    while ((userChoice < 8) || (userChoice > 128)) {
+        userChoice = parseInt(window.prompt("Enter the number of characters between 8 and 128: "));
+
+        if (isNaN(userChoice)) {
+
+            userChoice = 0;
+        }
+        return userChoice;
+
+    }
+
+
+
+
+}
 
 
 
